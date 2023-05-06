d689af592ab1f68eda0393160f4472e25baefa3a
NielReitmann <64618262+Niel-Reitmann@users.noreply.github.com>
2023-05-06 15:56:51 +1000

    blogsnotredirecting

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+import Posts from "./components/Posts"
+
 export default function Home() {
   return (
     <main className="px-6 mx-auto">
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Hello and Welcome &nbsp;
         <span className="whitespace-nowrap">
-          I'm <span className="font-bold"> Niel </span>
+          I&apos;m <span className="font-bold"> Niel </span>
         </span>
       </p>
+
+      <Posts />
       
     </main>
   )
--- lib/post.tsx
+++ lib/post.tsx
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
+import { remark } from 'remark'
+import html from 'remark-html'
 
 const postDirectory = path.join(process.cwd(), 'blogposts')
 
@@ -31,4 +33,29 @@ export function getSortedPostsData() {
     });
     // Sort posts by date
     return allPostsData.sort((a, b) => a.date < b.date ? 1 : -1);
+}
+
+export async function getPostData(id: string) {
+    const fullPath = path.join(postDirectory, '${id}.md');
+    const fileContents = fs.readFileSync(fullPath, 'utf-8');
+
+    // Use gray-matter to parse the post metadata secton
+    const matterResult = matter(fileContents);
+
+    const processedContent = await remark()
+        .use(html)
+        .process(matterResult.content);
+
+    const contentHTML = processedContent.toString();
+
+    const blogPostWithHTML: BlogPost & { contentHTML: string } = {
+
+        id,
+        title: matterResult.data.title,
+        date: matterResult.data.date,
+        contentHTML,
+    }
+
+    // Combine the date with the id
+    return blogPostWithHTML
 }
