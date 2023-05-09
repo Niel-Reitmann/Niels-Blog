cb2703720adaaeb4cbad7dead9d2e1bc73d2dbc6
NielReitmann <64618262+Niel-Reitmann@users.noreply.github.com>
2023-05-09 22:08:01 +1000

    progress

--- app/test.tsx
+++ app/test.tsx
@@ -1,92 +1,89 @@
 import Link from "next/link";
+import React from "react";
 
 export default function NielSite() {
   return (
-    <div className="container mx-auto">
-      <div className="flex flex-col items-center">
-        <div className="text-center">
-          <div className="text-xl">
+    <div className="container w-full min-h-screen text-gray-600 flex justify-center items-center">
+      <div className="flex flex-col items-center justify-center min-h-screen h-full w-960 gap-26 px-24 overflow-x-hidden">
+        <div className="flex flex-col gap-24 w-full">
+          <div className="text-4xl font-light introheader">
             My name is Niel Reitmann. I'm currently a Growth Associate at{" "}
             <a
               href="https://www.startmate.com/?utm_source=Google&utm_medium=NielWebsite&utm_campaign=NielWebsite"
-              className="text-indigo-500 hover:text-indigo-400"
+              className="text-black no-underline hover:underline transition duration-1000"
             >
               Startmate
             </a>
             . Previously design at{" "}
-            <a href="https://www.entrylevel.net/" className="text-indigo-500 hover:text-indigo-400">
+            <a href="https://www.entrylevel.net/" className="text-black no-underline hover:underline transition duration-1000">
               EntryLevel
             </a>
             ,{" "}
-            <a href="https://www.facebook.com/uqiesociety/" className="text-indigo-500 hover:text-indigo-400">
+            <a href="https://www.facebook.com/uqiesociety/" className="text-black no-underline hover:underline transition duration-1000">
               UQIES
             </a>{" "}
             Vice-President, Co-founder of{" "}
             <a
               href="https://drive.google.com/file/d/1qoGOsiCukKNY37tBBAq5Dx2K7zSIo5jv/view"
-              className="text-indigo-500 hover:text-indigo-400"
+              className="text-black no-underline hover:underline transition duration-1000"
             >
               Flaming Galahs
             </a>{" "}
             and{" "}
             <a
               href="https://drive.google.com/file/d/1ri0wYzXAbHVwrk_a10ITqaIVeT0sMwmH/view"
-              className="text-indigo-500 hover:text-indigo-400"
+              className="text-black no-underline hover:underline transition duration-1000"
             >
               Travelator
             </a>
             .
           </div>
         </div>
-        <div className="mt-8 mb-8">
+        <div className="flex flex-wrap justify-start items-center gap-24">
           <a
             href="/story.html"
-            className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded mr-2"
+            className="text-gray-600 text-base font-medium rounded-md border-none px-6 h-8 transition duration-200 ease-in relative inline-flex items-center shadow-sm hover:bg-gradient-to-r from-gray-200 to-transparent"
           >
             Story
           </a>
           <a
             href="https://www.linkedin.com/in/niel-reitmann/"
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mr-2"
+            className="text-gray-600 text-base font-medium rounded-md border-none px-6 h-8 transition duration-200 ease-in relative inline-flex items-center shadow-sm hover:bg-gradient-to-r from-gray-200 to-transparent"
           >
             LinkedIn
           </a>
           <a
             href="https://twitter.com/NielReitmann"
-            className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded mr-2"
+            className="text-gray-600 text-base font-medium rounded-md border-none px-6 h-8 transition duration-200 ease-in relative inline-flex items-center shadow-sm hover:bg-gradient-to-r from-gray-200 to-transparent"
           >
             Twitter
           </a>
           <a
             href="https://github.com/Niel-Reitmann"
-            className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mr-2"
+            className="text-gray-600 text-base font-medium rounded-md border-none px-6 h-8 transition duration-200 ease-in relative inline-flex items-center shadow-sm hover:bg-gradient-to-r from-gray-200 to-transparent"
           >
             GitHub
           </a>
-          <a
-            href="/blog.html"
-            className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
-          >
-            <Link href="/blogpage">Blog</Link>
-          </a>
+
+            <Link className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded" href="/blogpage">Blog</Link>
+
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
-          <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-            <div className="relative pb-48 overflow-hidden">
-              <img
-                className="absolute inset-0 h-full w-full object-cover"
-                src="/public/images/SmLogo.jpeg"
-                />
-                </div>
-                <div className="px-4 py-2">
+
+        <div className="w-full flex flex-wrap justify-between gap-16">
+          <div className="">
+            <div className="">
+            
+                <div className="text-gray-500 w-48 flex justify-center items-center">
+                    <img
+                    className="w-full"
+                    src="/images/SmLogo.jpeg"
+                    />
                   <h3 className="text-lg font-medium text-gray-900">
                     Startmate
                   </h3>
-                  <p className="text-gray-600 mt-1">
+                  <p className="text-base font-normal my-1">
                   The epicentre for startup ambition across Australia and New Zealand.
                   </p>
-                </div>
-                <div className="px-4 py-2">
                   <a
                     href="https://www.startmate.com/?utm_source=Google&utm_medium=NielWebsite&utm_campaign=NielWebsite"
                     className="text-indigo-500 hover:text-indigo-400"
@@ -94,73 +91,55 @@ export default function NielSite() {
                     Learn more
                   </a>
                 </div>
-              </div>
-              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                <div className="relative pb-48 overflow-hidden">
-                  <img
-                    className="absolute inset-0 h-full w-full object-cover"
-                    src="/public/images/Flaming Galah Logo.png"
-                  />
-                </div>
-                <div className="px-4 py-2">
-                  <h3 className="text-lg font-medium text-gray-900">
-                    Flaming Galahs
+                <div className="text-gray-500 w-48 flex justify-center items-center">
+                    <img
+                    className="w-full"
+                    src="/images/SmLogo.jpeg"
+                    />
+                  <h3 className="text-lg font-normal my-1">
+                    Travelator
                   </h3>
-                  <p className="text-gray-600 mt-1">
-                  A technology collective exclusively for Australia's university students.
+                  <p className="text-base font-normal my-1">
+                  The epicentre for startup ambition across Australia and New Zealand.
                   </p>
-                </div>
-                <div className="px-4 py-2">
                   <a
-                    href="https://galah.community/"
+                    href="https://www.startmate.com/?utm_source=Google&utm_medium=NielWebsite&utm_campaign=NielWebsite"
                     className="text-indigo-500 hover:text-indigo-400"
                   >
                     Learn more
                   </a>
                 </div>
-              </div>
-              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                <div className="relative pb-48 overflow-hidden">
-                  <img
-                    className="absolute inset-0 h-full w-full object-cover"
-                    src="/public/images/SmLogo.jpeg"
-                  />
-                </div>
-                <div className="px-4 py-2">
-                  <h3 className="text-lg font-medium text-gray-900">
-                    UQIES
+                <div className="text-gray-500 w-48 flex justify-center items-center">
+                    <img
+                    className="w-full"
+                    src="/images/SmLogo.jpeg"
+                    />
+                  <h3 className="text-lg font-normal my-1">
+                    Flaming Galahs
                   </h3>
-                  <p className="text-gray-600 mt-1">
-                  The most influential entrepreneurship society at The University of Queensland.
+                  <p className="text-base font-normal my-1">
+                  The epicentre for startup ambition across Australia and New Zealand.
                   </p>
-                </div>
-                <div className="px-4 py-2">
                   <a
-                    href="https://www.facebook.com/uqiesociety/"
+                    href="https://www.startmate.com/?utm_source=Google&utm_medium=NielWebsite&utm_campaign=NielWebsite"
                     className="text-indigo-500 hover:text-indigo-400"
                   >
                     Learn more
-                    </a>
+                  </a>
                 </div>
-              </div>
-              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                <div className="relative pb-48 overflow-hidden">
-                  <img
-                    className="absolute inset-0 h-full w-full object-cover"
-                    src="/public/images/SmLogo.jpeg"
-                  />
-                </div>
-                <div className="px-4 py-2">
-                  <h3 className="text-lg font-medium text-gray-900">
-                    Travelator
+                <div className="text-gray-500 w-48 flex justify-center items-center">
+                    <img
+                    className="w-full"
+                    src="/images/SmLogo.jpeg"
+                    />
+                  <h3 className="text-lg font-normal my-1">
+                    UQIES
                   </h3>
-                  <p className="text-gray-600 mt-1">
-                  Pioneering the future of tourism influencer marketing.
+                  <p className="text-base font-normal my-1">
+                  The epicentre for startup ambition across Australia and New Zealand.
                   </p>
-                </div>
-                <div className="px-4 py-2">
                   <a
-                    href="https://galah.community/"
+                    href="https://www.startmate.com/?utm_source=Google&utm_medium=NielWebsite&utm_campaign=NielWebsite"
                     className="text-indigo-500 hover:text-indigo-400"
                   >
                     Learn more
@@ -170,6 +149,8 @@ export default function NielSite() {
             </div>
           </div>
         </div>
+    </div>
+
   );
 
 }
